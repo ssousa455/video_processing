204c9d66b581b3549751fed81396ec031fa42d4c
Sérgio Sousa <40366240+ssousa455@users.noreply.github.com>
2023-10-02 16:14:58 -0300

    Refatoração para resolução de bug de aceleração e otimização de desempenho
    Neste commit, foram realizadas as seguintes alterações no script de processamento de vídeo:
    
    1. Refatoração da lógica de filtragem para utilizar o argumento `filter_complex`, permitindo uma cadeia de filtros complexa que pode lidar com múltiplas operações em uma única etapa. Isso resolve o problema onde alguns vídeos não estavam sendo acelerados corretamente ao selecionar a opção 3.
    2. Otimização do desempenho ao utilizar 75% dos núcleos disponíveis, evitando sobrecarregar o sistema enquanto ainda aproveita o processamento paralelo para acelerar o processamento dos vídeos.
    3. Adição de uma cláusula `finally` para garantir que os recursos do pool de processamento sejam liberados após a conclusão, evitando a possibilidade de vazamentos de memória ou processos órfãos.
    
    Essas melhorias visam não apenas corrigir bugs existentes, mas também otimizar o desempenho e garantir que o script seja robusto e confiável durante e após a execução.

--- video_processing 1.0.py
+++ video_processing 1.0.py
@@ -1,89 +1,72 @@
 import os
 import time
 import ffmpeg
+from multiprocessing import Pool, cpu_count
 
-def process_videos(option):
-    start_time = time.time()
-    processed_files = 0
-    base_path = os.getcwd()
+def process_video(args):
+    file, root, option = args
+    input_path = os.path.join(root, file)
+    output_suffix = ""
 
-    for root, dirs, files in os.walk(base_path):
-        for file in files:
-            if file.endswith(".mp4"):
-                input_path = os.path.join(root, file)
-                output_suffix = ""
+    probe = ffmpeg.probe(input_path)
+    video_stream = next(stream for stream in probe['streams'] if stream['codec_type'] == 'video')
+    height = video_stream['height']
 
-                if option == '1':
-                    output_suffix = "_accelerated"
-                    ffmpeg.input(input_path).output(
-                        input_path.replace('.mp4', f'{output_suffix}.mp4'),
-                        vf='setpts=0.5*PTS',
-                        af='atempo=2',
-                        vcodec='libx265',
-                        preset='ultrafast',
-                        crf='25',
-                        acodec='aac',
-                        ab='128k',
-                        threads='4'
-                    ).overwrite_output().run()
-                    processed_files += 1
+    vf_filter = ''
+    af_filter = ''
 
-                elif option == '2':
-                    output_suffix = "_cvt720p"
-                    ffmpeg.input(input_path).output(
-                        input_path.replace('.mp4', f'{output_suffix}.mp4'),
-                        vf='scale=-1:720',
-                        vcodec='libx265',
-                        preset='ultrafast',
-                        crf='25',
-                        acodec='aac',
-                        ab='128k',
-                        threads='4'
-                    ).overwrite_output().run()
-                    processed_files += 1
+    if option == '1' or option == '3':
+        vf_filter = 'setpts=0.5*PTS'
+        af_filter = 'atempo=2'
+        output_suffix = "_accelerated"
 
-                elif option == '3':
-                    output_suffix = "_accelerated_cvt720p"
-                    temp_output = input_path.replace('.mp4', '_temp_accelerated.mp4')
-                    ffmpeg.input(input_path).output(
-                        temp_output,
-                        vf='setpts=0.5*PTS',
-                        af='atempo=2',
-                        vcodec='libx265',
-                        preset='ultrafast',
-                        crf='25',
-                        acodec='aac',
-                        ab='128k',
-                        threads='4'
-                    ).overwrite_output().run()
+    if (option == '2' or option == '3') and height >= 1080:
+        vf_filter = f'{vf_filter},scale=-1:720' if vf_filter else 'scale=-1:720'
+        output_suffix = f'{output_suffix}_cvt720p' if output_suffix else '_cvt720p'
 
-                    ffmpeg.input(temp_output).output(
-                        input_path.replace('.mp4', f'{output_suffix}.mp4'),
-                        vf='scale=-1:720',
-                        vcodec='libx265',
-                        preset='ultrafast',
-                        crf='25',
-                        acodec='aac',
-                        ab='128k',
-                        threads='4'
-                    ).overwrite_output().run()
-                    os.remove(temp_output)
-                    processed_files += 1
+    filter_complex = f'{vf_filter};{af_filter}'
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-
-    print(f"Processamento concluído. {processed_files} arquivos processados em {elapsed_time:.2f} segundos.")
-    print(f"Processo iniciado na pasta: {base_path}")
+    ffmpeg.input(input_path).output(
+        input_path.replace('.mp4', f'{output_suffix}.mp4'),
+        filter_complex=filter_complex,
+        vcodec='libx265',
+        preset='ultrafast',
+        crf='25',
+        acodec='aac',
+        ab='128k',
+        threads='4'
+    ).overwrite_output().run()
 
 if __name__ == '__main__':
-    choice = input("Escolha uma opção (1, 2 ou 3):\n"
-                   "1. Acelerar vídeos\n"
-                   "2. Converter vídeos para 720p\n"
-                   "3. Acelerar e converter vídeos para 720p\n"
-                   "Opção: ")
+    try:
+        choice = input("Escolha uma opção (1, 2 ou 3):\n"
+                       "1. Acelerar vídeos\n"
+                       "2. Converter vídeos para 720p\n"
+                       "3. Acelerar e converter vídeos para 720p\n"
+                       "Opção: ")
 
-    if choice not in ['1', '2', '3']:
-        print("Opção inválida. Por favor, escolha novamente.")
-    else:
-        process_videos(choice)
+        if choice not in ['1', '2', '3']:
+            print("Opção inválida. Por favor, escolha novamente.")
+        else:
+            start_time = time.time()
+            base_path = os.getcwd()
+            all_files = []
+            for root, dirs, files in os.walk(base_path):
+                for file in files:
+                    if file.endswith(".mp4"):
+                        all_files.append((file, root, choice))
+
+            # Use 75% dos cores disponíveis para evitar sobrecarregar o sistema
+            with Pool(int(cpu_count() * 0.75)) as pool:
+                pool.map(process_video, all_files)
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            processed_files = len(all_files)
+
+            print(f"Processamento concluído. {processed_files} arquivos processados em {elapsed_time:.2f} segundos.")
+            print(f"Processo iniciado na pasta: {base_path}")
+    finally:
+        # Isso garantirá que a pool seja fechada e todos os recursos sejam liberados após a conclusão
+        pool.close()
+        pool.join()
